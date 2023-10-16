f41fa24393ad65a8d3e66a457d91708063600c2b
TauruS111 <134150044+TauruS111@users.noreply.github.com>
2023-10-16 18:03:09 +0000

    Deploying to gh-pages from @ TauruS111/hometask-9-js@24854ae3c30d43f7fac17fb1e08cda98830f3642 🚀

---
+++ 01-color-switcher.c5c369e2.js
@@ -0,0 +1,2 @@
+let e=null;const t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");d.disabled=!0,t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),d.addEventListener("click",(()=>{t.disabled=!1,d.disabled=!0,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.c5c369e2.js.map
